477117771ae53a4481aa59221c1baa16ecf08fa3
Kerry W. Lothrop <kerry@lothrop.de>
2015-12-07 22:31:10 +0100

    Updated demo to use XIB file.

--- DwxDemo.iOS/Views/FirstView.cs
+++ DwxDemo.iOS/Views/FirstView.cs
@@ -1,51 +1,27 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <summary>
-//    Defines the FirstView type.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Touch.Views;
+using DwxDemo.Core.ViewModels;
+using UIKit;
+
 namespace DwxDemo.iOS.Views
 {
-    using Cirrious.MvvmCross.Binding.BindingContext;
-
-    using Core.ViewModels;
-    using CoreGraphics;
-    using Foundation;
-    using UIKit;
-
-    /// <summary>
-    /// Defines the FirstView type.
-    /// </summary>
-    [Register("FirstView")]
-    public class FirstView : BaseView
+    public partial class FirstView : MvxViewController
     {
-        /// <summary>
-        /// Views the did load.
-        /// </summary>
-        /// <summary>
-        /// Called when the View is first loaded
-        /// </summary>
+        public FirstView() : base("FirstView", null)
+        {
+        }
+
         public override void ViewDidLoad()
         {
-            this.View = new UIView { BackgroundColor = UIColor.White };
-
             base.ViewDidLoad();
 
-            UILabel uiLabel = new UILabel(new CGRect(10, 10, 300, 40));
-            View.AddSubview(uiLabel);
-            UITextField uiTextField = new UITextField(new CGRect(10, 50, 300, 40));
-            View.AddSubview(uiTextField);
-            UIButton uiButton = new UIButton(new CGRect(10, 90, 300, 40));
-            uiButton.SetTitle("Download", UIControlState.Normal);
-            uiButton.SetTitleColor(UIColor.Blue, UIControlState.Normal);
-            View.AddSubview(uiButton);
-
             var set = this.CreateBindingSet<FirstView, FirstViewModel>();
-            set.Bind(uiLabel).To(vm => vm.MyProperty);
-            set.Bind(uiTextField).To(vm => vm.MyProperty);
-            set.Bind(uiButton).To(vm => vm.MyCommand);
+            set.Bind(UiLabel).To(vm => vm.MyProperty);
+            set.Bind(UiTextField).To(vm => vm.MyProperty);
+            set.Bind(UiButton).To(vm => vm.MyCommand);
             set.Apply();
 
-            UITapGestureRecognizer tap = new UITapGestureRecognizer(() => uiTextField.ResignFirstResponder());
+            var tap = new UITapGestureRecognizer(() => UiTextField.ResignFirstResponder());
             View.AddGestureRecognizer(tap);
         }
     }
